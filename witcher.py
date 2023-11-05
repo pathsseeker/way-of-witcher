046bce3e22c5343b9a422f00db8583b7c303a3d1
Pathsseeker <qaliozero@yandex.ru>
2023-11-05 17:49:10 +0300

    Add save reading

--- witcher.py
+++ witcher.py
@@ -1,6 +1,14 @@
-print("Hello, stranger.")
-name = input("What is your name?\n")
-print(f"Hello, {name}.")
+import sh
 
-with open("Save", "w") as fd:
-    fd.write(name)
+save = sh.find('.', '-type', 'f', '-name', 'Save').rstrip('\n')
+if not save:
+    print("Hello, stranger.")
+    name = input("What is your name?\n")
+    print(f"Hello, {name}.")
+
+    with open("Save", "w") as fd:
+        fd.write(name)
+else:
+    with open(save) as fd:
+        name = fd.readline().rstrip()
+    print(f'Nice to meet you again, {name}')
